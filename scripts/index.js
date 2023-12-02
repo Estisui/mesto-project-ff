5921e143b500174b772dc4fab17eb6162acdbd11
Stepan Sukhachev <droiderlinux@yahoo.com>
2023-12-02 19:18:06 +0300

    feat: add cards creation+removal+render

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+// Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+// DOM узлы
+const cardsContainer = document.querySelector(".places__list");
+// Функция создания карточки
+const createCard = (cardValues, deleteCard) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardValues.link;
+  cardElement.querySelector(".card__title").textContent = cardValues.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+  return cardElement;
+};
+// Функция удаления карточки
+const deleteCard = (e) => {
+  const cardElement = e.target.closest(".card");
+  cardElement.remove();
+};
+// Вывести карточки на страницу
+initialCards.forEach((card) => {
+  cardsContainer.append(createCard(card, deleteCard));
+});
