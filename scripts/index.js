54f5dc89255a35e2385a97e41e881ddd4b485c3b
Stepan Sukhachev <droiderlinux@yahoo.com>
2023-12-03 14:14:12 +0300

    refactor: add alt attr + change card delete logic

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,22 @@ const cardTemplate = document.querySelector("#card-template").content;
 // DOM узлы
 const cardsContainer = document.querySelector(".places__list");
 // Функция создания карточки
-const createCard = (cardValues, deleteCard) => {
+const createCard = (cardData, onDelete) => {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  cardElement.querySelector(".card__image").src = cardValues.link;
-  cardElement.querySelector(".card__title").textContent = cardValues.name;
+  const cardImage = cardElement.querySelector(".card__image");
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
   cardElement
     .querySelector(".card__delete-button")
-    .addEventListener("click", deleteCard);
+    .addEventListener("click", () => onDelete(cardElement));
   return cardElement;
 };
 // Функция удаления карточки
-const deleteCard = (e) => {
-  const cardElement = e.target.closest(".card");
-  cardElement.remove();
+const onDelete = (card) => {
+  card.remove();
 };
 // Вывести карточки на страницу
 initialCards.forEach((card) => {
-  cardsContainer.append(createCard(card, deleteCard));
+  cardsContainer.append(createCard(card, onDelete));
 });
